0076894a21b5badf85c3b1d302627666e6b09777
Dan Philibin <dan@danphilibin.com>
2022-06-17 15:22:45 -0700

    remove express dependency

---
+++ index.ts
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+import Interval, { io } from "@interval/sdk";
+
+// Google Cloud Run requires a process listening on port 8080
+const http = require("http");
+const server = http.createServer(() => {});
+server.listen(8080);
+
+const interval = new Interval({
+  apiKey: process.env.INTERVAL_API_KEY,
+  actions: {
+    hello_world: async () => {
+      const name = await io.input.text("Your name");
+      return `Hello, ${name}`;
+    },
+  },
+});
+
+interval.listen();
